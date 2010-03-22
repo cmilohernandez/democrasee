8f2721e8d798b55a9c79b3e6aec7d77b51800cc8
hubert.c.liu@gmail.com <hubert.c.liu@gmail.com@9932dfe9-535c-9e3c-bcc2-8151383f497d>
2010-03-22 22:05:19 +0000

    added bill model

--- trunk/democrasee/billviewer/models.py
+++ trunk/democrasee/billviewer/models.py
@@ -1,5 +1,20 @@
 from google.appengine.ext import db
 
 # Create your models here.
-#class Congressman(db.Model):
+class Congressman(db.Model):
+	name = db.StringProperty()
+	district = db.StringProperty()
+	party = db.StringProperty()
+	email = db.StringProperty()
+	phone_number = db.PhoneNumberProperty()
+	twitter = db.StringProperty()
+	created_on = db.DateTimeProperty(auto_now_add = 1)
+	
+class Bill(db.Model):
+	intro_date = db.DateProperty()
+	status = db.StringProperty()
+	abstract = db.TextProperty()
+	bill_number = db.StringPropery() #ex: HB615
+	#committee = db.StringProperty()
+	#TODO: add content_root, sponsors, committee
 	
