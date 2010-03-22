a7449eb99f7b1f28b23db026e773e21db599afa3
hubert.c.liu <hubert.c.liu@gmail.com>
2010-03-22 21:19:11 +0000

    fixed root_path shit

--- democrasee/settings.py
+++ democrasee/settings.py
@@ -63,11 +63,13 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'democrasee.urls'
 
+import os
+ROOT_PATH = os.path.dirname(__file__)
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".  Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	ROOT_PATH + '/templates',
+    ROOT_PATH + '/templates',
 )
 
 INSTALLED_APPS = (
