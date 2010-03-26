5f5136573694b673a485ca472d308be441c988ce
hubert.c.liu <hubert.c.liu@gmail.com>
2010-03-26 03:34:39 +0000

    layout... sorta done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :bills
 
   map.resources :congressmen
+  
+  map.root :controller => "dashboard", :action => "index"
     
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
