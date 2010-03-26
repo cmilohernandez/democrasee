78e10e81eca2c513e2539ca83237cabbad3a7948
hubert.c.liu@gmail.com <hubert.c.liu@gmail.com@9932dfe9-535c-9e3c-bcc2-8151383f497d>
2010-03-26 00:11:13 +0000

    models finished i think

--- trunk/app/models/bill.rb
+++ trunk/app/models/bill.rb
@@ -2,5 +2,6 @@ class Bill < ActiveRecord::Base
   
   has_and_belongs_to_many :congressmen
   has_many :votes
+  has_many :sentences
   
 end
--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  Clearance::Routes.draw(map)
+  
   map.resources :bills
 
   map.resources :congressmen
