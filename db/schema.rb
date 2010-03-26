d62a2f9d99ea9b039e9999089125be98b40cbdb3
hubert.c.liu <hubert.c.liu@gmail.com>
2010-03-26 00:11:13 +0000

    models finished i think

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100323013350) do
+ActiveRecord::Schema.define(:version => 20100325015306) do
 
   create_table "bills", :force => true do |t|
     t.text     "content"
@@ -38,6 +38,38 @@ ActiveRecord::Schema.define(:version => 20100323013350) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sentences", :force => true do |t|
+    t.boolean  "isRaw"
+    t.text     "text"
+    t.integer  "votes_up"
+    t.integer  "votes_down"
+    t.integer  "flag_votes"
+    t.integer  "user_id"
+    t.integer  "sentence_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "bill_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "username"
+    t.string   "political_affiliation"
+    t.boolean  "male"
+    t.date     "dob"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "encrypted_password",    :limit => 128
+    t.string   "salt",                  :limit => 128
+    t.string   "confirmation_token",    :limit => 128
+    t.string   "remember_token",        :limit => 128
+    t.boolean  "email_confirmed",                      :default => false, :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["id", "confirmation_token"], :name => "index_users_on_id_and_confirmation_token"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
   create_table "votes", :force => true do |t|
     t.date     "date"
